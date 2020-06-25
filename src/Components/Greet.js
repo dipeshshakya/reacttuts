7ed3d5b79a1eb9f2caef1a1e4001fdc877857a0a
dipesh shakya <dipeshshakya106@gmail.com>
2020-06-25 20:42:39 +1000

    functional components

---
+++ src/Components/Greet.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+// function Greet() {
+//   return (
+//     <div>
+//       <h1>Dipesh</h1>
+//     </div>
+//   );
+// }
+const Greet = () => <h1>Dipesh</h1>;
+
+export default Greet;
