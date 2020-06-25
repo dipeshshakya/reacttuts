5aecd371548840741d1f831defc1392cba709222
dipesh shakya <dipeshshakya106@gmail.com>
2020-06-25 20:58:36 +1000

    class component

---
+++ src/Components/Welcome.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+export class Welcome extends Component {
+  render() {
+    return (
+      <div>
+        <h1>
+          Welcome
+          <br />
+        </h1>
+      </div>
+    );
+  }
+}
+
+export default Welcome;
