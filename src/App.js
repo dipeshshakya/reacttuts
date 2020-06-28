cfdc87fc5e817e439c2563cc439ae966229168c6
dipesh shakya <dipeshshakya106@gmail.com>
2020-06-28 10:18:02 +1000

    setState methods

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 import Greet from "./Components/Greet";
 import Welcome from "./Components/Welcome";
 import Message from "./Components/Message";
+import Counter from "./Components/Counter";
 
 function App() {
   return (
@@ -13,7 +14,8 @@ function App() {
         <Greet name="Dipesh" heroName="Shaktiman">
           <p>This the paragraph tag</p>
         </Greet> */}
-        <Message />
+        {/* <Message /> */}
+        <Counter />
       </header>
     </div>
   );
