012f5af67f145d96235d56396c2ae91ac8e302fb
dipesh shakya <dipeshshakya106@gmail.com>
2020-06-28 11:16:47 +1000

    passing methods as props

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 // import logo from './logo.svg';
 import "./App.css";
-import Greet from "./Components/Greet";
-import Welcome from "./Components/Welcome";
-import Message from "./Components/Message";
-import Counter from "./Components/Counter";
-import FunctionClick from "./Components/FunctionClick";
-import ClassClick from "./Components/ClassClick";
-import EventBind from "./Components/EventBind";
+// import Greet from "./Components/Greet";
+// import Welcome from "./Components/Welcome";
+// import Message from "./Components/Message";
+// import Counter from "./Components/Counter";
+// import FunctionClick from "./Components/FunctionClick";
+// import ClassClick from "./Components/ClassClick";
+// import EventBind from "./Components/EventBind";
+import ParentComponent from "./Components/ParentComponent";
 
 function App() {
   return (
@@ -21,7 +22,8 @@ function App() {
         {/* <Counter /> */}
         {/* <FunctionClick />
         <ClassClick /> */}
-        <EventBind />
+        {/* <EventBind /> */}
+        <ParentComponent />
       </header>
     </div>
   );
